75682245a8d14c4918c80af6c992212acc8b4265
Bart Wijnants <bart.w@sparkcentral.com>
2020-05-12 21:01:49 +0200

    update all dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle, { colors } from "../global-style";
 import App from "../components/app";
