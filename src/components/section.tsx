3f8bb23e495d8f72d4ec2479a9d53c3951a2fad9
Bart Wijnants <bart.w@sparkcentral.com>
2020-05-14 20:45:04 +0200

    fresh start

--- src/components/section.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { spacing } from "../global-style";
-
-const Section = styled.section`
-  margin-top: ${spacing.large};
-`;
-
-export default Section;
